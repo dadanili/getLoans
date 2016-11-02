4c440a4c5c6b5c665981226db3272e1011659246
dadanili <yli523@gmail.com>
2016-11-01 18:14:39 -0700

    added http GET request to service for getting loans

--- app/services/services.js
+++ app/services/services.js
@@ -1,4 +1,14 @@
 angular.module('loans', [])
-.factory('services', function(){
-	
+.factory('services', function($http){
+	return {
+		getLoans: function() {
+			return $http({
+				method: 'GET', 
+				url: 'http://api.kivaws.org/v1/loans/newest.json'
+			})
+			.then(function(response){
+				console.log('got response', response)
+			})
+		}
+	}
 })
